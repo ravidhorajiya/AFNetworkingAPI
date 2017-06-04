29a65fa64788135d53f628042d1de120b615d2ed
Ravi Dhorajiya <dhorajiya104@gmail.com>
2017-06-04 14:38:22 +0530

    Initial Commit

---
+++ AFNetworkingAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingAPI
+//
+//  Created by Ravi Dhorajiya on 04/06/17.
+//  Copyright © 2017 Ravi Dhorajiya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingAPI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AFNetworkingAPI
+//
+//  Created by Ravi Dhorajiya on 04/06/17.
+//  Copyright © 2017 Ravi Dhorajiya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
